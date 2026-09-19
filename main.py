35cf841d9c6cefaff1c6d0897cb590a1c20dcee1
Tornzu <timur.something.p@gmail.com>
2026-09-19 18:01:43 +0700

    feat: add menu cart and add to basket

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+from Product import Product
+
 load_dotenv()
 token = os.getenv('TOKEN')
 
@@ -11,31 +13,59 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    else:
-        await bot.process_commands(message)
-        await message.channel.send(message.content)
+products = [
+    Product(name="Bread", price="$3", description="Flat sliced white bread"),
+    Product(name="Milk", price="$2", description="0% fat milk"),
+    Product(name="Cheese", price="$4", description="dairy free cheese"),
+]
+
+basket = []
 
 @bot.command()
-async def button(ctx):
+async def start(ctx):
+    await ctx.send("Welcome to shop market")
 
+
+
+@bot.command()
+async def shop(ctx):
+    message = ""
     view = discord.ui.View()
+    for product in products:
+        async def callback(interaction: discord.Interaction, product=product):
+            await interaction.response.send_message(f"{product.name}: {product.price},{product.description}",
+                                                    view=view2)
 
-    async def button_callback(interaction: discord.Interaction):
-        await interaction.response.send_message("You clicked the button")
+        async def addbasket_callback(interaction: discord.Interaction, product=product):
+            basket.append(product)
+            await interaction.response.send_message(f"Product {product.name} added to basket")
 
-    button = discord.ui.Button(
-        label="Click me",
-        style=discord.ButtonStyle.blurple,
-    )
+        message += f"{product.name}: {product.price}\n"
 
-    button.callback = button_callback
-    view.add_item(button)
+        button1 = discord.ui.Button(
+            label=product.name,
+            style=discord.ButtonStyle.green
+        )
+
+        view2 = discord.ui.View()
+
+        addBasketButton = discord.ui.Button(
+            label="add to basket",
+            style=discord.ButtonStyle.green
+        )
+        addBasketButton.callback = addbasket_callback
+        view2.add_item(addBasketButton)
 
 
-    await ctx.send("Click the button", view=view)
+        button1.callback = callback
+        view.add_item(button1)
+
+    message += "Select a product: "
+
+    await ctx.send(message, view=view)
+
+
+
+
 
 bot.run(token)
